0c7387120750bd443981ceb3d7cba4427e8bb3bd
Martin Jobin <mjobin123@yahoo.co.uk>
2021-02-17 18:34:02 +0100

    Created Show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database...'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+puts 'Done!'
+
+puts 'Creating 3 ingredients...'
+
+Ingredient.create!(name: 'lemon')
+Ingredient.create!(name: 'ice')
+Ingredient.create!(name: 'mint leaves')
+
+puts "Seeded #{Ingredient.count} ingredients"
+
+puts 'Finished'
